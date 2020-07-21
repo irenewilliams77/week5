06da26989d5470e33af11fa3c9bc9a74ae78d8d7
Irene Williams <europeandistribution@ymail.com>
2020-07-21 12:00:24 -0400

    fail

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var assert =('assert');
+var assert= require ('../triangle.js');
+
+describe('triangle', function(){
+    it('correctly calculates its area', function (){
+    assert.equal(t.area()).to.equal(25);
+)});
--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
-var side1= 7;
-var side2= 3;
-var side3= 5'
+var side1= 5;
+var side2= 1;
+var side3= 9;
 
 var s =(side1 +side2 + side3)/2;
 var area = Math.sqrt(s*((side1)*(side2)*(side3)));
