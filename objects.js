f2ccb09341171e07d3aa6e8e40d2e1c349a58d06
Irene Williams <europeandistribution@ymail.com>
2020-07-22 11:02:57 -0400

    created file, tested function

--- objects.js
+++ objects.js
@@ -17,3 +17,21 @@ var renaissanceWomenInScience = {
 console.log(renaissanceWomenInScience.buyNow());
 
 
+var renaissanceWomenInScience = {
+
+    title: "Renaissance Women In Science",
+    authorName: "Louise Q. Van Der Does and Rita J. Simon",
+    genre:"biography",
+    shortDescription: "Biograpy of various women scientists ranging from 1818 to 1994. How their discoveries impacted science today",
+    price: "10",
+    seller: "University Press of America, Inc. ",
+
+    essence :function(){
+        return this.authorName + this.genre + this.shortDescription;
+    
+    
+    }
+        
+}
+console.log(renaissanceWomenInScience.essence());
+
